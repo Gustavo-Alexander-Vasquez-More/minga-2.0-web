f043b41bf1debee67f0b58c5834564ea57e644fc
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-08-18 18:28:47 -0300

    mejorado del index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,11 @@ export default function index() {
   return (
     <div className='bg-[url("/Background.png")] w-full h-[60vh]'>
     <Navbar/>
-    <div className=' w-full h-[45vh] bg-[#ff00002a] flex justify-center items-center ]'>
+    <div className=' w-full h-[50vh] flex-col flex gap-2 px-[4rem]'>
+      <h1 className=' text-[white] text-[4rem] font-bold mt-[1.2rem]'>For the love of manga</h1>
+      <p className=' text-[white] text-[1.5rem]'>Explore our varieties</p>
+      <p className=' text-[white] text-[1.2rem]'>#MingaLove❤</p>
+      <button  className='bg-[white] w-[12rem] h-[3rem] text-[#ffbb3d] text-[1.2rem] font-bold rounded-[5px]'>Sign in!</button>
     </div>
     <Carrousel/>
     </div>
