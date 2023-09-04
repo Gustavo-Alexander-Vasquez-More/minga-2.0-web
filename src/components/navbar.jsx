3f50b9b57e4ad68daecd82dc95dafef6dbdc218d
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-09-04 03:06:16 -0300

    arreglos

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -101,9 +101,12 @@ console.log(photoUser);
               </>
             ) : (
               <>
-                <Anchor to='/' className='hover:text-[#131313] text-[white]'>Home</Anchor>
-                <Anchor to='footer' className='hover:text-[#131313]  text-[white]'>View Mangas</Anchor>
-                
+                <Anchor to='/' className='hover:text-[#131313] text-[white] flex gap-3'><svg class="w-6 h-6 text-[white] dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9"/>
+  </svg>Home</Anchor>
+  <Anchor to='/mangas' className='hover:text-[#131313]  text-[white] flex  gap-3'><svg class="w-6 h-6 text-[white] dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+  </svg>View Mangas</Anchor>
                 <Anchor to='/LogIn' className='hover:text-[#131313]  text-[white]'>Sign In</Anchor>
                 <Anchor to='/Register' className='hover:text-[#131313]  text-[white]'>Register</Anchor>
               </>
